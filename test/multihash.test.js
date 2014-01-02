d1c6694378b93a9727ec70cf120d251a1fac7764
Jared Hanson <jaredhanson@gmail.com>
2014-01-02 10:43:28 -0800

    Add more test cases.

--- test/multihash.test.js
+++ test/multihash.test.js
@@ -27,4 +27,74 @@ describe('MultiHash', function() {
     });
   });
   
+  describe('with two single-value elements', function() {
+    var hash = new MultiHash();
+    hash.put('hello', 'world');
+    hash.put('foo', 'bar');
+    
+    it('should have length of two', function() {
+      expect(hash.length).to.equal(2);
+    });
+    
+    it('should have present keys', function() {
+      expect(hash.has('hello')).to.be.true;
+      expect(hash.count('hello')).to.equal(1);
+      
+      expect(hash.has('foo')).to.be.true;
+      expect(hash.count('foo')).to.equal(1);
+    });
+    
+    it('should yield key-value pairs when iterating', function() {
+      var obj = [];
+      hash.forEach(function(key, value) {
+        obj.push({ key: key, value: value });
+      });
+      
+      expect(obj).to.have.length(2);
+      expect(obj[0].key).to.equal('hello');
+      expect(obj[0].value).to.equal('world');
+      expect(obj[1].key).to.equal('foo');
+      expect(obj[1].value).to.equal('bar');
+    });
+  });
+  
+  describe('with one multi-value element', function() {
+    var hash = new MultiHash();
+    hash.put('foo', 'bar');
+    hash.put('foo', 'baz');
+    
+    it('should have length of two', function() {
+      expect(hash.length).to.equal(1);
+    });
+    
+    it('should have present key', function() {
+      expect(hash.has('foo')).to.be.true;
+      expect(hash.count('foo')).to.equal(2);
+    });
+    
+    it('should yield key-value pairs when iterating', function() {
+      var obj = [];
+      hash.forEach(function(key, value) {
+        obj.push({ key: key, value: value });
+      });
+      
+      expect(obj).to.have.length(2);
+      expect(obj[0].key).to.equal('foo');
+      expect(obj[0].value).to.equal('bar');
+      expect(obj[1].key).to.equal('foo');
+      expect(obj[1].value).to.equal('baz');
+    });
+    
+    it('should yield last key-value pair when iterating with flatten option', function() {
+      var obj = [];
+      hash.forEach(function(key, value) {
+        obj.push({ key: key, value: value });
+      }, { flatten: true });
+      
+      expect(obj).to.have.length(1);
+      expect(obj[0].key).to.equal('foo');
+      expect(obj[0].value).to.equal('baz');
+    });
+  });
+  
 });
